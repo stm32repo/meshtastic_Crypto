c505d2174add9aa2c38672d52acdb088f7842a5d
Thomas Göttgens <tgoettgens@gmail.com>
2026-09-16 15:08:53 +0200

    XEdDSA: negate the private scalar without ref10 sc_muladd (#4)
    priv_curve_to_ed_keys used ref10 sc_muladd only to compute -a mod q.
    Compute it with reduceQ and BigNumberUtil primitives instead, remove both
    sc_muladd copies and the ZERO/MINUS_ONE tables, and clean a2/ptA2 on exit.
    
    Output is byte-identical to the previous implementation.
    XEdDSA.o text (arm-none-eabi-g++ 9.3.1, -Os, Cortex-M4): 12086 -> 4390 bytes.

--- XEdDSA.h
+++ XEdDSA.h
@@ -3,15 +3,6 @@
 
 // imported from SUPERCOP by Daniel J. Bernstein which is public domain
 
-// Byte-representation of the scalar value of 0 on the Ed25519 curve. Needed by `sc_neg`.
-static const uint8_t ZERO[32] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-
-// Byte-representation of the scalar value of -1 on the Ed25519 curve. Needed by `sc_neg`.
-static const uint8_t MINUS_ONE[32] = {0xec, 0xd3, 0xf5, 0x5c, 0x1a, 0x63, 0x12, 0x58, 0xd6, 0x9c, 0xf7,
-                                      0xa2, 0xde, 0xf9, 0xde, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10};
-
 typedef uint64_t crypto_uint64;
 typedef int64_t crypto_int64;
 
@@ -22,8 +13,6 @@ typedef crypto_int32 fe[10];
 static crypto_uint64 load_3(const unsigned char *in);
 
 static crypto_uint64 load_4(const unsigned char *in);
-
-void sc_muladd(unsigned char *s, const unsigned char *a, const unsigned char *b, const unsigned char *c);
 
 void fe_sub(fe h, fe f, fe g);
 
@@ -48,7 +37,6 @@ public:
     static void sign(uint8_t signature[64], const uint8_t privateKey[32],
                    const uint8_t publicKey[32], const void *message, size_t len);
 private:
-        static void sc_muladd(unsigned char *s, const unsigned char *a, const unsigned char *b, const unsigned char *c);
         static void deriveKeys(SHA512 *hash, limb_t *a, const uint8_t privateKey[32]);
 
 
